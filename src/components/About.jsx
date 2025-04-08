2fbb7f8f1056d8878b2a77f0b808175f829e5542
naimovdavlatshoh <you@example.com>
2025-04-08 13:34:02 +0500

    ...

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React from "react";
 import Throphy from "../assets/throphy.png";
 import Kreml from "../assets/kreml.jpg";
-import LogoBus from "../assets/logobus.jpg";
+import LogoBus from "../assets/logobuss.jpg";
 import Coloboration from "../assets/coloboration.png";
 
 export const metadata = {
